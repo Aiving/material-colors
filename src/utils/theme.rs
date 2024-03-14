0b385e3a3443923b3f5f4d245ef563b13ca5faee
Allan Zhang <allanzhang7@gmail.com>
2024-03-14 13:45:41 -0400

    feat(api)!: rename color functions
    This renames two functions to be more idiomatic, in line with 0.2 API
    changes.
    
    - `theme_from_source_color` replaced with `Theme::from_source_color`
    - `custom_color` replaced with `CustomColorGroup::new`
    
    Both are breaking.

--- src/utils/theme.rs
+++ src/utils/theme.rs
@@ -37,6 +37,44 @@ pub struct CustomColorGroup {
     pub dark: ColorGroup,
 }
 
+impl CustomColorGroup {
+    /// Generate custom color group from source and target color
+    ///
+
+    ///
+    /// @link https://m3.material.io/styles/color/the-color-system/color-roles
+    fn new(source: Argb, color: CustomColor) -> Self {
+        let mut value = color.value;
+
+        let from = value;
+        let to = source;
+
+        if color.blend {
+            value = harmonize(from, to);
+        }
+
+        let palette = CorePalette::of(value);
+        let tones = palette.primary;
+
+        Self {
+            color,
+            value,
+            light: ColorGroup {
+                color: tones.tone(40),
+                on_color: tones.tone(100),
+                color_container: tones.tone(90),
+                on_color_container: tones.tone(10),
+            },
+            dark: ColorGroup {
+                color: tones.tone(80),
+                on_color: tones.tone(20),
+                color_container: tones.tone(30),
+                on_color_container: tones.tone(90),
+            },
+        }
+    }
+}
+
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct Schemes {
@@ -64,77 +102,33 @@ pub struct Theme {
     pub custom_colors: Vec<CustomColorGroup>,
 }
 
-/// Generate a theme from a source color
-///
-/// @param source Source color
-///
-/// @param customColors Array of custom colors
-///
-/// @return Theme object
-pub fn theme_from_source_color(source: Argb, custom_colors: Vec<CustomColor>) -> Theme {
-    let palette = CorePalette::of(source);
+impl Theme {
+    /// Generatse a theme from a source color
+    pub fn from_source_color(source: Argb, custom_colors: Vec<CustomColor>) -> Theme {
+        let palette = CorePalette::of(source);
 
-    Theme {
-        source,
-        schemes: Schemes {
-            light: SchemeTonalSpot::new(source.into(), false, None)
-                .scheme
-                .into(),
-            dark: SchemeTonalSpot::new(source.into(), true, None)
-                .scheme
-                .into(),
-        },
-        palettes: Palettes {
-            primary: palette.primary,
-            secondary: palette.secondary,
-            tertiary: palette.tertiary,
-            neutral: palette.neutral,
-            neutral_variant: palette.neutral_variant,
-            error: palette.error,
-        },
-        custom_colors: custom_colors
-            .into_iter()
-            .map(|c| custom_color(source, c))
-            .collect(),
-    }
-}
-
-/// Generate custom color group from source and target color
-///
-/// @param source Source color
-///
-/// @param color Custom color
-///
-/// @return Custom color group
-///
-/// @link https://m3.material.io/styles/color/the-color-system/color-roles
-fn custom_color(source: Argb, color: CustomColor) -> CustomColorGroup {
-    let mut value = color.value;
-
-    let from = value;
-    let to = source;
-
-    if color.blend {
-        value = harmonize(from, to);
-    }
-
-    let palette = CorePalette::of(value);
-    let tones = palette.primary;
-
-    CustomColorGroup {
-        color,
-        value,
-        light: ColorGroup {
-            color: tones.tone(40),
-            on_color: tones.tone(100),
-            color_container: tones.tone(90),
-            on_color_container: tones.tone(10),
-        },
-        dark: ColorGroup {
-            color: tones.tone(80),
-            on_color: tones.tone(20),
-            color_container: tones.tone(30),
-            on_color_container: tones.tone(90),
-        },
+        Theme {
+            source,
+            schemes: Schemes {
+                light: SchemeTonalSpot::new(source.into(), false, None)
+                    .scheme
+                    .into(),
+                dark: SchemeTonalSpot::new(source.into(), true, None)
+                    .scheme
+                    .into(),
+            },
+            palettes: Palettes {
+                primary: palette.primary,
+                secondary: palette.secondary,
+                tertiary: palette.tertiary,
+                neutral: palette.neutral,
+                neutral_variant: palette.neutral_variant,
+                error: palette.error,
+            },
+            custom_colors: custom_colors
+                .into_iter()
+                .map(|c| CustomColorGroup::new(source, c))
+                .collect(),
+        }
     }
 }
