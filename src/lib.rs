af26c93602b05aa35e14275810c41de69021e319
Allan Zhang <allanzhang7@gmail.com>
2024-03-15 01:24:24 -0400

    feat: enhance Clippy linting directives
    And for my final act, I introduce *lib.rs*.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![allow(
+    // pedantic lints
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::many_single_char_names,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::unreadable_literal,
+    clippy::used_underscore_binding,
+    clippy::similar_names,
+    // pedantic lints for later
+    clippy::too_many_lines,
+    clippy::doc_markdown,
+    clippy::missing_panics_doc,
+    clippy::implicit_hasher,  // we use ahash on Scheme
+    clippy::suboptimal_flops, // some more cases can be optimized
+    // nursery lints for later
+    clippy::large_stack_frames,
+    clippy::cognitive_complexity
+)]
+
 pub mod blend;
 pub mod color;
 pub mod contrast;
